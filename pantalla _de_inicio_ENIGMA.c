a3aaf4aa6f6c56522297e8f58ae46ffdf6577609
jbustilloe <47812975+jbustilloe@users.noreply.github.com>
2019-04-05 13:33:54 +0200

    actualizacion
    adjunto progrma actualizado con la seleccion de la secuencia inicial mejorada y la codificacion de las letras una a una.

--- pantalla _de_inicio_ENIGMA.c
+++ pantalla _de_inicio_ENIGMA.c
@@ -7,73 +7,109 @@ while (1)
 {
 	printf("\t BIENVENIDO A ENIGMA \n");
 	int o=0;
+	int s=0;
 	int a=0, b=0, c=0;
 	while (o<1||o>2)
 		{
 		printf("escoja una opcion:\n 1.cifrar \t 2.descifrar\n");
-		scanf("%i", &o);	
+		scanf("%i", &o);
+		if (o<1||o>2)
+			{
+				printf ("opcion no valida, ");
+			}	
 		}
 	switch(o)
 	{
 	case 1:
 		printf("has escogido la opcion de cifrar\n");
-			while (a<1||a>9)
+			printf ("Escribe las tres claves (a[0,9], b[0,9], c[1,3].)\n");
+			printf ("a= ");
+			scanf ("%d", &a);
+			while (a<0 || a>9)
+			{
+				printf ("numero no valido\na= ");
+				scanf ("%d", &a);
+			}
+	
+			printf ("b= ");
+			scanf ("%d", &b);
+			while (b<0 || b>9)
+			{
+				printf ("numero no valido\nb=");
+				scanf ("%d", &b);
+			}
+	
+			printf ("c= ");
+			scanf ("%d", &c);
+			while (c<1 || c>3)
+			{
+				printf ("numero no valido\nc=");
+				scanf ("%d", &c);
+			}
+			printf ("posicion inicial de las ruedas:\n (%d, %d, %d)\n", c, b, a);
+			
+		printf ("teclee cualquier tecla para iniciar\n");
+	scanf ("%c\n", &s);
+	while (1)
+	{
+			a ++;
+			if (a == 10)
+			{
+				a = 0;
+				b ++;
+				if (b == 10)
 				{
-				printf("introduzca un numero del 1 al 9, el primer numero de la secuencia inicial:\n");
-				scanf("%i", &a);
-				if (a<1||a>9)
+					b = 0;
+					c ++;
+					if (c == 4)
 					{
-						printf ("numero no valido, ");
-					}	
+						c = 1;
+					}
 				}
-			while (b<1||b>9)
-				{
-				printf("introduzca un numero del 1 al 9, el segundo numero de la secuencia inicial:\n");
-				scanf("%i", &b);
-				if (b<1||b>9)
-					{
-						printf ("numero no valido, ");
-					}	
-				}
-			while (c<1||c>3)
-				{
-				printf("introduzca un numero del 1 al 3, el tercer numero de la secuencia inicial\n");
-				scanf("%i", &c);
-				if (a<1||c>3)
-					{
-						printf ("numero no valido, ");
-					}	
-				}
-			break;
+			}
+		s = (s+a+b)*c; 
+		while (s > 126)
+		{
+			s = s - (126);
+		}
+		if (s < 32)
+		{
+			s = s + 32;
+		}
+		printf ("(%d, %d, %d)\n", c, b, a);
+		printf (" new= %c %d\n", s, s);
+		scanf ("%c\n", &s);
+	}
+			
+	break;
 	case 2:
-		printf("has escogido la opcion de descifrar\n");
-			while (a<1||a>9)
-				{
-				printf("introduzca un numero del 1 al 9, el primer numero de la secuencia inicial:\n");
-				scanf("%i", &a);
-				if (a<1||a>9)
-					{
-						printf ("numero no valido, ");
-					}	
-				}
-			while (b<1||b>9)
-				{
-				printf("introduzca un numero del 1 al 9, el segundo numero de la secuencia inicial:\n");
-				scanf("%i", &b); 
-				if (b<1||b>9)
-					{
-						printf ("numero no valido, ");
-					}	
-				}
-			while (c<1||c>3)
-				{
-				printf("introduzca un numero del 1 al 3, el tercer numero de la secuencia inicial\n");
-				scanf("%i", &c);
-				if (a<1||c>3)
-					{
-						printf ("numero no valido, ");
-					}	
-				}
+				printf("has escogido la opcion de descifrar\n");
+					printf ("Escribe las tres claves (a[0,9], b[0,9], c[1,3].)\n");
+			
+			printf ("a= ");
+			scanf ("%d", &a);
+			while (a<0 || a>9)
+			{
+				printf ("numero no valido\na= ");
+				scanf ("%d", &a);
+			}
+	
+			printf ("b= ");
+			scanf ("%d", &b);
+			while (b<0 || b>9)
+			{
+				printf ("numero no valido\nb=");
+				scanf ("%d", &b);
+			}
+	
+			printf ("c= ");
+			scanf ("%d", &c);
+			while (c<1 || c>3)
+			{
+				printf ("numero no valido\nc=");
+				scanf ("%d", &c);
+			}
+			printf ("posicion inicial de las ruedas:\n (%d, %d, %d)\n", c, b, a);
 	
 	break;
 	}
