148fa1e136aedd1586a4588226f67b21dc6cbe42
jesusmarcostorero <47813395+jesusmarcostorero@users.noreply.github.com>
2019-05-09 18:06:11 +0200

    depurado de la funcion aleatoria

--- rand+codificado.c
+++ rand+codificado.c
@@ -1,30 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include <string.h>
+#define X 100
+
+typedef struct//la unidad del vector de alfabetos
+{
+	char unidad[26];
+}alfabeto;
+
 void main()
 {
-	FILE *alfabeto;
-	char *ruedas[9][9][3][26];
-	char rueda1[26];
+	int i;
+	FILE *pf;
+	alfabeto vector[X];
 	printf ("iniciando secuencia aleatoria de alfabeto.\n");
-	alfabetos();//escribe al empezar la secuencia aleatoria de los alfabetos en el fichero
+	rand_alfabetos();//escribe al empezar la secuencia aleatoria de los alfabetos en el fichero
 	printf ("secuencia realizada con exito.\n");
-	alfabeto= fopen("alfabetos.odt", "r");
-	
-	fscanf(alfabeto, "%26s", rueda1);//guarda la primera rueda en un char
-	printf ("Rueda 1: %26s\n", rueda1);
-	fclose (alfabetos);
 	
 }
 
 
-int alfabetos()
+int rand_alfabetos()
+/*escribe un numero determinado de alfabetos aleatorios en un fichero
+para leerlos o copiarlos a una matriz mas tarde*/
 {
 	FILE *pf;
-	int rn,rc;//variables para numeros aleatrorios//
-	int a,b,c,d; //variables de la rueda + letra//
-	int comp[25]; //matriz auxiliar de comparación de igualdad//
-	int i,n,e; //variables auxiliares/
+	int rn,rc;//variables para numeros aleatrorios
+	int a; //variable para el bucle de matrices
+	int d; //variable para el bucle de alfabetos
+	int comp[25]; //matriz auxiliar de comparación de igualdad
+	int i,n,e; //variables auxiliares
 	srand((unsigned)time(NULL));
 	pf=fopen("alfabetos.odt","w");
 	if(pf==NULL){
@@ -33,21 +39,13 @@ int alfabetos()
 	}
 	else
 	{
-		for(a=0;a<10;a++)
+		for(a=0; a<X; a++)
 		{
-			for(b=0;b<10;b++)
-			{
-				for(c=0;c<3;c++)
-				{
-					for(e=0;e<25;e++)
-					{
-						comp[e]=0;//devolvemos el vector comparador a todo valores 0//
-					}
-					for(d=0;d<25;d++)//aquí es donde se genera el alfabeto//
+			for(d=0;d<25;d++)//aquí es donde se genera el alfabeto//
 					{
 						n=0;
 						rn=rand();
-						if(rn>122||rn<97)
+						if(rn>122||rn<97)//si el numero aleatorio no esta entre la a y la z, se repite//
 						{
 							d--;
 						}
@@ -57,7 +55,7 @@ int alfabetos()
 							{
 								if(comp[i]==rn)//cuenta el numero de veces que se repite un numero//
 								{
-									n++;
+									n++;//flag para ver las repeticiones
 								}
 							}
 							if(n>0)//si se repite con alguno, se deja y d se resta uno para que cree otro numero hasta que no sea repetido//
@@ -72,9 +70,6 @@ int alfabetos()
 							}	
 						}
 					}
-				fprintf (pf, "\n", rc);	
-				}
-			}
 		}
 		fclose(pf);
 	}
