177cc07de381c4ae9b5c63b939535257edba5ec9
jesusmarcostorero <47813395+jesusmarcostorero@users.noreply.github.com>
2019-05-10 00:27:50 +0200

    matriz y principio del encoder
    diseño de recogida de datos terminado, generacion de alfabetos y matrices por la misma constante, y principio del algoritmo de codificacion

--- rand+codificado.c
+++ rand+codificado.c
@@ -9,14 +9,45 @@ typedef struct//la unidad del vector de alfabetos
 	char unidad[26];
 }alfabeto;
 
+void copia (int i, alfabeto *vector);
+
 void main()
 {
-	int i;
-	FILE *pf;
-	alfabeto vector[X];
-	printf ("iniciando secuencia aleatoria de alfabeto.\n");
+	char s[100];
+	int n;
+	int len=0;
+	int i=0;
+	int b=80;
+	alfabeto vector;//alfabeto para copiar del fichero
+	alfabeto *pvector = &vector;//puntero al alfabeto
+	alfabeto matriz[b];//matriz de alfabetos
+	printf ("iniciando secuencia aleatoria de alfabeto....\n");
 	rand_alfabetos();//escribe al empezar la secuencia aleatoria de los alfabetos en el fichero
 	printf ("secuencia realizada con exito.\n");
+	for (i=0; i<b; i++)
+	{
+		copia (i, &vector);
+		strcpy (matriz[i].unidad, vector.unidad);
+		
+	}
+	printf ("\n");
+	printf ("introduce una frase\n");
+	scanf ("%100s", s);
+	len = strlen(s);
+	printf ("%d\n", len);
+	for (i=0; i<len; i++)
+	{
+		if (s[i]>'a' && s[i]<'z')//si la letra esta contenida en el rango del alfabeto, se codifica
+		{
+			n = s[i] - 97;
+			printf ("%c", matriz[i].unidad[n]);
+		}
+		else //si la letra no esta contenida, se deja como esta
+		{
+			printf ("%c", s[i]);
+		}
+	}
+	
 	
 }
 
@@ -30,7 +61,7 @@ para leerlos o copiarlos a una matriz mas tarde*/
 	int a; //variable para el bucle de matrices
 	int d; //variable para el bucle de alfabetos
 	int comp[25]; //matriz auxiliar de comparación de igualdad
-	int i,n,e; //variables auxiliares
+	int i,n; //variables auxiliares
 	srand((unsigned)time(NULL));
 	pf=fopen("alfabetos.odt","w");
 	if(pf==NULL){
@@ -42,36 +73,52 @@ para leerlos o copiarlos a una matriz mas tarde*/
 		for(a=0; a<X; a++)
 		{
 			for(d=0;d<25;d++)//aquí es donde se genera el alfabeto//
+			{
+				n=0;
+				rn=rand();
+				
+				if(rn>122||rn<97)//si el numero aleatorio no esta entre la a y la z, se repite//
+				{
+					d--;
+				}
+				
+				else
+				{
+					for(i=0;i<d;i++)
 					{
-						n=0;
-						rn=rand();
-						if(rn>122||rn<97)//si el numero aleatorio no esta entre la a y la z, se repite//
+						if(comp[i]==rn)//cuenta el numero de veces que se repite un numero//
 						{
-							d--;
-						}
-						else
-						{
-							for(i=0;i<d;i++)
-							{
-								if(comp[i]==rn)//cuenta el numero de veces que se repite un numero//
-								{
-									n++;//flag para ver las repeticiones
-								}
-							}
-							if(n>0)//si se repite con alguno, se deja y d se resta uno para que cree otro numero hasta que no sea repetido//
-							{
-								d--;
-							}
-							else
-							{
-								comp[d]=rn;
-								rc=(char)rn;//convertimos el numero en letra//
-								fprintf(pf,"%c", rc);
-							}	
+							n++;//flag para ver las repeticiones
 						}
 					}
+					if(n>0)//si se repite con alguno, se deja y d se resta uno para que cree otro numero hasta que no sea repetido//
+					{
+						d--;
+					}
+					else
+					{
+						comp[d]=rn;
+						rc=(char)rn;//convertimos el numero en letra//
+						fprintf(pf,"%c", rc);
+					}	
+				}
+			}
 		}
 		fclose(pf);
 	}
 	return(0);	
+}
+
+void copia(int i, alfabeto *vector)
+/*devuelve segun el numero i que se le ponga el alfabeto
+correspondiente a su posición en el fichero*/
+{	
+	FILE *pf;
+	pf = fopen ("alfabetos.odt", "r");
+	if (i > 0)//en el caso de un numero superior, la lectura avanza 26 char y empieza a leer desde alli.
+	{
+		fseek (pf, i*sizeof(char)*26, SEEK_SET);
+	}
+	fscanf(pf, "%26s", vector->unidad);
+	fclose(pf);
 }
